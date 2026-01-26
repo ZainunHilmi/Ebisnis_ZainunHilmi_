d189602ab07770f8217a3e26ef09b69728c3ff54
Miboyy <mzhilmi1612@gmail.com>
2026-01-26 15:25:10 +0700

    fix: adjust app service provider and bridge for vercel compatibility

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            config(['view.compiled' => '/tmp/storage/framework/views']);
+        }
     }
 }
