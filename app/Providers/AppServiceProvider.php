02dea3368b0cd2e4e28a3412f22692b1593984ac
Miboyy <mzhilmi1612@gmail.com>
2026-01-27 05:43:32 +0700

    fix: restore config/view.php and clean up AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
-            config(['view.compiled' => '/tmp/storage/framework/views']);
-        }
+        //
     }
 }
