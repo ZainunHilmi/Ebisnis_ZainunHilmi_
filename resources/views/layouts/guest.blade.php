9ef87fc5f3a3d7bb738e2d4666609b542ea900e6
Miboyy <mzhilmi1612@gmail.com>
2026-01-29 15:08:57 +0700

    fix: synchronize guest and welcome layouts with new Vite asset configuration

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
         * {
